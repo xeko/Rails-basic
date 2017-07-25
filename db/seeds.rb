556b0a750d3178407d82f69ebc42f92002472eb5
System Administrator <root@miyashiroriona-no-MacBook-Air.local>
2017-07-25 16:40:36 +0900

    Lam quen voi Rails

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Todo.create!(title: 'grocery shopping', notes: 'pickles, eggs, red onion')
+Todo.create!(title: 'wash the car')
+Todo.create!(title: 'register kids for school', notes: 'Register Kira for Ruby Junior High and Caleb for Rails High School')
+Todo.create!(title: 'check engine light', notes: 'The check engine light is on in the Tacoma')
+Todo.create!(title: 'dog groomers', notes: 'Take Pinky and Redford to the groomers on Wednesday the 23rd')
